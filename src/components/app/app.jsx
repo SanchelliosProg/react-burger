a7d476b325f861b368bd4ef9c1364a542e18a6cc
SanchelliosProg <vasyania2009@gmail.com>
2021-06-30 22:37:58 +0300

    - now we are fetching data from api

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,34 @@
-import React from "react";
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 import style from "./app.module.css";
-import data from "../../utils/data.js";
 import chosen from "../../utils/chosen.js";
+import { useState } from "react";
+import { useEffect } from "react";
+
+const url = "https://norma.nomoreparties.space/api/ingredients";
 
 const App = () => {
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    fetch(url, { method: "GET" })
+      .then((resp) => resp.json())
+      .then((body) => {
+        console.log('fetched data', body.data)
+        setIngredients(body.data);
+        console.log('new state', ingredients);
+      }).catch(e => {
+        console.log('Error while fetching data: ', e);
+      });
+  }, []);
+
   return (
     <>
       <AppHeader />
       <main>
         <section className={`${style.ingredients}`}>
-          <BurgerIngredients data={data} />
+          <BurgerIngredients data={ingredients} />
         </section>
         <section className={`pr-10`}>
           <BurgerConstructor chosen={chosen} />
