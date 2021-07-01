547857299c70e7add8e167483612c0a14d457a73
SanchelliosProg <vasyania2009@gmail.com>
2021-07-01 10:34:53 +0300

    - made starter for modal

--- src/components/burger-item/burger-item.jsx
+++ src/components/burger-item/burger-item.jsx
@@ -1,19 +1,46 @@
-import { CurrencyIcon, Counter } from "@ya.praktikum/react-developer-burger-ui-components";
+import {
+  CurrencyIcon,
+  Counter,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 import style from "./burger-item.module.css";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import { useState } from "react";
+import Modal from "../modal/modal";
 
 const BurgerItem = (props) => {
-  const counter = props.chosen.filter(choice => (choice.name === props.item.name)).length;
+  const counter = props.chosen.filter(
+    (choice) => choice.name === props.item.name
+  ).length;
+  const [isModalOpened, setModalState] = useState(false);
+  const toggleModalState = () => {
+    console.log("toggleModalState in BurgerItem is called");
+    setModalState(!isModalOpened);
+  };
+
   return (
-    <div className={`${style.container} ${props.rightPadding}`}>
-      {counter > 0 ? <Counter count={counter} size="default"/> : undefined}
-      <img src={props.item.image} alt="Кусок бургера" className="ml-4 mr-4 mb-1"/>
-      <div className={`${style.price} mb-1`}>
-        <span className={`text text_type_digits-default pr-1`}>{props.item.price}</span>
-        <CurrencyIcon type="primary" />
+    <>
+      {isModalOpened && <Modal onClose={toggleModalState}/>}
+      <div
+        className={`${style.container} ${props.rightPadding}`}
+        onClick={toggleModalState}
+      >
+        {counter > 0 ? <Counter count={counter} size="default" /> : undefined}
+        <img
+          src={props.item.image}
+          alt="Кусок бургера"
+          className="ml-4 mr-4 mb-1"
+        />
+        <div className={`${style.price} mb-1`}>
+          <span className={`text text_type_digits-default pr-1`}>
+            {props.item.price}
+          </span>
+          <CurrencyIcon type="primary" />
+        </div>
+        <span className={`${style.name} text text_type_main-default`}>
+          {props.item.name}
+        </span>
       </div>
-      <span className={`${style.name} text text_type_main-default`}>{props.item.name}</span>
-    </div>
+    </>
   );
 };
 
@@ -21,6 +48,6 @@ BurgerItem.propTypes = {
   chosen: PropTypes.arrayOf(PropTypes.object),
   item: PropTypes.object,
   rightPadding: PropTypes.string,
-}
+};
 
 export default BurgerItem;
