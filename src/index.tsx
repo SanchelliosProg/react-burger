0c01952a46a24efd6513cd01ed9b9fafd3041f69
SanchelliosProg <vasyania2009@gmail.com>
2021-07-15 08:34:26 +0300

    - enabled Redux Dev Tools

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/app/app.jsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/app/app.jsx";
+import thunk from "redux-thunk";
+import { compose, createStore, applyMiddleware } from "redux";
+import { rootReducer } from "./services/reducers";
+import { Provider } from "react-redux";
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+const store = createStore(rootReducer, enhancer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
